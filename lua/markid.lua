59d21de98d2d3c1a32e916a974bcd3f1d967f283
D065023 <david.kunz@sap.com>
2022-09-11 11:04:24 +0200

    better colors

--- lua/markid.lua
+++ lua/markid.lua
@@ -72,9 +72,7 @@ function M.init()
         return pcall(vim.treesitter.parse_query, lang, config.queries[lang] or config.default_query)
       end,
 
-      colors = {
-        "#bf6060", "#bf9f60", "#bcbf60", "#60bf91", "#60b7bf", "#607cbf", "#7860bf", "#a160bf", "#bf6093"
-      },
+      colors = { '#619e9d', '#9E6162', '#81A35C', '#7E5CA3', '#9E9261', '#616D9E', '#97687B', '#689784', '#999C63', '#66639C', '#967869', '#698796', '#9E6189', '#619E76' },
 
       queries = {
         javascript = [[
