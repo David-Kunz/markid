951c1acbdfb824e5a6ec1c0b819df2b3de8e175d
Dr. David A. Kunz <david.kunz@sap.com>
2022-11-11 10:30:36 +0100

    feat: added javascript shorthand_property_identifier

--- lua/markid.lua
+++ lua/markid.lua
@@ -34,6 +34,7 @@ M.queries = {
           (identifier) @markid
           (property_identifier) @markid
           (shorthand_property_identifier_pattern) @markid
+          (shorthand_property_identifier) @markid
         ]]
 }
 M.queries.typescript = M.queries.javascript
